a9dc756dc1d2e798528b5bbd0bbbcca915fd8f34
miftahuljannahhh <mj5416386@gmail.com>
2025-05-08 10:29:34 +0700

    void addnode

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -2,19 +2,60 @@
 #include <string.h>
 using namespace std;
 
-class Node {
-    public:
+class Node
+{
+public:
     int noMhs;
     Node *next;
 };
 
-class list {
+class List
+{
     Node *START;
 
     public:
-    list() {
+    List()
+    {
         START = NULL;
     }
 
-   
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if ((START == NULL) || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current = NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+
 }
